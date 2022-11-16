15ad178d0000e622d47254f9d41397e477d2627f
GAP <90244749+ziven8807@users.noreply.github.com>
2022-11-16 15:33:45 +0800

    Delete 島島阿學/Member directory

--- 島島阿學/Member/src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-
-import App from './App';
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
